398abc2476366a47e3abefa779a6a1d732052cc4
rtk_7jsq <WarThunder74@yandex.ru>
2025-04-20 06:13:07 +0300

    add soft

--- kde.sh
+++ kde.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo pacman -S plasma firefox obs-studio vlc ffmpeg kio-admin ncdu noto-fonts-cjk telegram-desktop dolphin konsole ffmpegthumbs grub-customizer spectacle htop evince libreoffice libreoffice-fresh-ru code kdeconnect ark easytag power-profiles-daemon kcalc openssh
+sudo pacman -S plasma firefox obs-studio vlc ffmpeg kio-admin ncdu noto-fonts-cjk telegram-desktop dolphin konsole ffmpegthumbs grub-customizer spectacle htop evince libreoffice libreoffice-fresh-ru code kdeconnect ark easytag power-profiles-daemon kcalc openssh elisa gimp
 
 sudo systemctl enable sddm
 
